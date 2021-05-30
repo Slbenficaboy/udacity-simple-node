07b27066a76acde1ddc2e69cb6eec0cf100fe93c
Eduardo Cerqueira <slbenficaboy@gmail.com>
2021-05-30 14:18:04 -0400

    Add an env variable reference for travis testing.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ function sleep(ms) {
 }
 
 async function main() {
+  const favoriteTeam = process.env.FAVORITE_TEAM;
   while(true) {
+    console.log(`My favorite team is ${favoriteTeam}`);
     console.log('Containers rule!');
     await sleep(5000);
   }
